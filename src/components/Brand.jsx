a24000dba1147df80a48f9f0f3982cb52ed054d3
OracleAlsanna <byeagerbusiness@gmail.com>
2026-09-09 19:07:19 -0500

    Change brand mark to a twinkling white star

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,9 +1,17 @@
 import './Brand.css';
 
+function IconSparkle() {
+  return (
+    <svg className="brand-star" viewBox="0 0 24 24" fill="#ffffff" aria-hidden="true">
+      <path d="M12 2 L14 10 L22 12 L14 14 L12 22 L10 14 L2 12 L10 10 Z" />
+    </svg>
+  );
+}
+
 export default function Brand() {
   return (
     <div className="brand">
-      <span className="brand-star" aria-hidden="true" />
+      <IconSparkle />
       <span className="brand-name">oracle</span>
     </div>
   );
